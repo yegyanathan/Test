b1ef3f8c21c13d28bb417b98d4d3b3efe3734ce8
Yegyanathan V <58477218+yegyanathan@users.noreply.github.com>
2024-03-16 18:11:02 +0530

    Update landingpage.js

--- landingpage.js
+++ landingpage.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './styles.css'; // Import CSS file for styling
 
 const LandingPage = () => {
   return (
-    <div>
+    <div className="landing-page">
       <h1>Welcome to the Event Management Platform</h1>
-      <Link to="/signup">Sign Up</Link>
-      <Link to="/login/admin">Login as Admin</Link>
-      <Link to="/login/user">Login as User</Link>
+      <div className="buttons-container">
+        <Link to="/signup" className="button">Sign Up</Link>
+        <Link to="/login/admin" className="button">Login as Admin</Link>
+        <Link to="/login/user" className="button">Login as User</Link>
+      </div>
     </div>
   );
 };
